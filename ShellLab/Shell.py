aa3ff491eab18ec0257c12f07f1dfcf1f537e649
ealara3 <ealara3@miners.utep.edu>
2018-09-18 19:03:03 -0600

    Done

---
+++ ShellLab/Shell.py
@@ -0,0 +1,167 @@
+#! /usr/bin/env python3
+
+import os, sys, time, re
+
+
+def userChoice(userIN):
+    pid = os.getpid()               # get and remember pid
+
+
+    os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+
+    #rc = os.fork()
+
+    # if rc < 0:
+    #os.write(2, ("fork failed, returning %d\n" % rc).encode())
+    #sys.exit(1)
+
+    # elif rc == 0:                   # child
+    # os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
+    #  (os.getpid(), pid)).encode())
+    # args = ["wc", "p3-exec.py"]
+
+    if '<' in userIN:
+        if len(userIN) == 3:
+            args = [userIN[0], userIN[2]]
+            ScreenExec(args)
+        else:
+            args = [userIN[0],userIN[3]]
+            OutUser =  userIN[1]
+            fileExec(args,OutUser)
+
+    elif '>' in userIN:
+        args = [userIN[0], userIN[1]]
+        OutUser = userIN[3]
+        fileExec(args, OutUser)
+
+    elif '|' in userIN:
+        j=0
+        for i in userIN:
+            if i == "|":
+                break
+            j=j+1
+        LeftPart = userIN[0: j]
+        print(LeftPart)
+        RightPart = userIN[(j+1):]
+        print(RightPart)
+        pippingHere(LeftPart, RightPart)
+
+    else:
+        ScreenExec(userIN)
+
+            # os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+        #sys.exit(1)                 # terminate with error
+
+   # else:                           # parent (forked ok)
+        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
+                     (pid, rc)).encode())
+        #childPidCode = os.wait()
+        #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
+                   #childPidCode).encode())
+
+
+def pippingHere(Left, Right):
+    rc2 = os.fork()
+
+    if rc2 < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+
+    elif rc2 == 0:
+        if '<' in Left:
+            sys.exit(1)
+        elif '>' in Right:
+            sys.exit()
+        else:
+            #print("Doing work")
+            PipeLeft(Left)
+           # print("finish work")
+    else:
+        #print("waiting")
+        os.wait()
+        PipeRight(Right)
+        #print("continue")
+
+def PipeLeft(Left):
+    #print("I arrive")
+    os.close(1)             #close the screen
+    os.dup(w)
+    for i in (r,w):
+        os.close(i)
+
+
+    fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+    os.set_inheritable(fd, True)
+
+    os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+        program = "%s/%s" % (dir, Left[0])
+        try:
+            os.execve(program, Left, os.environ)  # try to exec program
+        except FileNotFoundError:  # ...expected
+            pass  # ...fail quietly
+
+
+
+
+def PipeRight(Right):
+    #print("HALLO!!!!!!!")
+    os.close(0)             #close keyboard
+    os.dup(r)
+    for i in (r,w):
+        os.close(i)
+
+
+    fd = sys.stdin.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+    os.set_inheritable(fd, True)
+
+    os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+        program = "%s/%s" % (dir, Right[0])
+        try:
+            os.execve(program, Right, os.environ)  # try to exec program
+        except FileNotFoundError:  # ...expected
+            pass  # ...fail quietly
+
+
+
+
+
+def ScreenExec(args):
+    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+        program = "%s/%s" % (dir, args[0])
+        try:
+            os.execve(program, args, os.environ)  # try to exec program
+        except FileNotFoundError:  # ...expected
+            pass  # ...fail quietly
+
+def fileExec(args, OutUser):
+    os.close(1)  # redirect child's stdout
+    #sys.stdout = open("p4-output.txt", "w")
+    sys.stdout = open(OutUser, "w")
+    fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+    os.set_inheritable(fd, True)
+    os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+        program = "%s/%s" % (dir, args[0])
+        try:
+            os.execve(program, args, os.environ)  # try to exec program
+        except FileNotFoundError:  # ...expected
+            pass  # ...fail quietly
+########################################################################################### main
+
+while 1 :
+   r, w = os.pipe()
+   userIN = input("").split()
+   #userIN = input("").split()
+
+   if userIN[0] == "exit":
+        break
+   else:
+       userChoice(userIN)
+      # if len(userIN) == 2:
+      #     Right = userIN[0]
+     #      Left = userIN[1]
+     #  else:
+    #       newuserIn = userIN.split()
+    #       userChoice(newuserIn)
--- ShellLab/ShellFork.py
+++ ShellLab/ShellFork.py
@@ -2,82 +2,174 @@
 
 import os, sys, time, re
 
-userIN = input("").split()
+while 1 :               #run always
+    hasPipe = False                         #This two variable will be used to do the second part of the pipe
 
-pid = os.getpid()               # get and remember pid
+    userIN = input("").split("|")
+    rightPipe = userIN
 
-os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
-
-rc = os.fork()
-
-if rc < 0:
-    os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
-
-elif rc == 0:                   # child
-    #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
-                 #(os.getpid(), pid)).encode())
-   # args = ["wc", "p3-exec.py"]
-    args =[userIN[0], userIN[1]]
-
-    #os.close(1)                 # redirect child's stdout
-    #sys.stdout = open("Name.txt", "w")
-    #fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-    #os.set_inheritable(fd, True)
-
-    #os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
-    if len(userIN) < 3:                             #if is only (ex) wc file.txt
-      for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly
-
-    elif len(userIN) == 3:                          #if is only (ex) wc < file.txt
-        if userIN[1] == "<":
-            args[1] = userIN[2]
-            for dir in re.split(":", os.environ['PATH']):  # try each directory in path
-                program = "%s/%s" % (dir, args[0])
-                try:
-                    os.execve(program, args, os.environ)  # try to exec program
-                except FileNotFoundError:  # ...expected
-                    pass
+    if len(userIN) == 2:
+        rightPipe = userIN[1].split()
+        userIN = userIN[0].split()
+        hasPipe = True
+    else:
+        userIN = userIN[0].split()
 
 
-    elif userIN[2] == ">":                          #if is only (ex) wc file.txt > H.txt
-         os.close(1)
-         sys.stdout = open(userIN[3], "w")
-         fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-         os.set_inheritable(fd, True)
-         for dir in re.split(":", os.environ['PATH']):  # try each directory in path
-             program = "%s/%s" % (dir, args[0])
-             try:
-                os.execve(program, args, os.environ)  # try to exec program
-             except FileNotFoundError:  # ...expected
-                 pass
+    if userIN[0] == 'exit':
+        break
 
-    elif userIN[2] == "<":                          #if is only (ex) wc H.txt < file.txt
-        os.close(1)
-        args = [userIN[0], userIN[3]]
-        sys.stdout = open(userIN[1], "w")
-        fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
-        os.set_inheritable(fd, True)
-        for dir in re.split(":", os.environ['PATH']):  # try each directory in path
-            program = "%s/%s" % (dir, args[0])
-            try:
-                os.execve(program, args, os.environ)  # try to exec program
-            except FileNotFoundError:  # ...expected
-                pass
+    else:                               #start the shell
+        r, w = os.pipe()
+
+        pid = os.getpid()               # get and remember pid
+
+        os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+
+        rc2 = -1
 
 
+        #for i in userIN:
+        #    if i == "|":
+        #         hasPipe =True
 
-    os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-    sys.exit(1)                 # terminate with error
 
-else:                           # parent (forked ok)
-    #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
-                # (pid, rc)).encode())
-    childPidCode = os.wait()
-    #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
-                # childPidCode).encode())
+        rc = os.fork()
+
+        if rc < 0:
+            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            sys.exit(1)
+
+        elif rc == 0:                   # child
+            #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
+            #(os.getpid(), pid)).encode())
+            # args = ["wc", "p3-exec.py"]
+
+            args =[userIN[0], userIN[1]]
+
+            #os.close(1)                 # redirect child's stdout
+            #sys.stdout = open("Name.txt", "w")
+            #fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+            #os.set_inheritable(fd, True)
+
+            #os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+            if len(userIN) < 3:                             #if is only (ex) wc file.txt
+                if hasPipe:
+                    # os.close(1)
+                    args = [userIN[0], userIN[1]]
+                    #  sys.stdout = open(userIN[1], "w")
+                    #  fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+                    # os.set_inheritable(fd, True)
+                    os.close(r)
+                    w = os.fdopen(w, 'w')
+                    fd = sys.stdout.fileno()
+                    w.write("hello")
+                    w.close()
+                    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+                        program = "%s/%s" % (dir, args[0])
+                        try:
+                            os.execve(program, args, os.environ)  # try to exec program
+                        except FileNotFoundError:  # ...expected
+                            pass
+                else:
+                    for dir in re.split(":", os.environ['PATH']): # try each directory in path
+                        program = "%s/%s" % (dir, args[0])
+                        try:
+                            os.execve(program, args, os.environ) # try to exec program
+                        except FileNotFoundError:             # ...expected
+                            pass                              # ...fail quietly
+
+
+            elif len(userIN) == 3:                          #if is only (ex) wc < file.txt
+                if userIN[1] == "<":
+                    args[1] = userIN[2]
+                    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+                        program = "%s/%s" % (dir, args[0])
+                        try:
+                            os.execve(program, args, os.environ)  # try to exec program
+                        except FileNotFoundError:  # ...expected
+                            pass
+
+            # elif userIN[1] == "|":
+
+
+            elif len(userIN) == 4:
+                if userIN[2] == ">":  # if is only (ex) wc file.txt > H.txt
+                    os.close(1)
+                    sys.stdout = open(userIN[3], "w")
+                    fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+                    os.set_inheritable(fd, True)
+                    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+                        program = "%s/%s" % (dir, args[0])
+                        try:
+                            os.execve(program, args, os.environ)  # try to exec program
+                        except FileNotFoundError:  # ...expected
+                            pass
+
+
+                elif userIN[2] == "<":  # if is only (ex) wc H.txt < file.txt
+                    os.close(1)
+                    args = [userIN[0], userIN[3]]
+                    sys.stdout = open(userIN[1], "w")
+                    fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+                    os.set_inheritable(fd, True)
+                    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+                        program = "%s/%s" % (dir, args[0])
+                        try:
+                            os.execve(program, args, os.environ)  # try to exec program
+                        except FileNotFoundError:  # ...expected
+                            pass
+
+                else:
+                    os.close(1)
+                    args = [userIN[0], userIN[3]]
+                    #  sys.stdout = open(userIN[1], "w")
+                    #  fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+                    # os.set_inheritable(fd, True)
+                    os.close(r)
+                    w = os.fdopen(w, 'w')
+                    fd = sys.stdout.fileno()
+                    w.write(fd)
+                    w.close()
+                    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
+                        program = "%s/%s" % (dir, args[0])
+                        try:
+                            os.execve(program, args, os.environ)  # try to exec program
+                        except FileNotFoundError:  # ...expected
+                            pass
+
+
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)                 # terminate with error
+
+        else:                           # parent (forked ok)
+            #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
+            # (pid, rc)).encode())
+            childPidCode = os.wait()
+            if hasPipe:  # if it has a pipe then fork ###############################################################
+                rc2 = os.fork()
+                # print(" I enter to pipe")
+                # print("wait")
+                # Parentpipe = os.wait()
+                # print("WAIT2")
+
+            if rc2 == 0:  # second part of the pipe
+                os.write(2, ("got error %d\n" % rc2).encode())
+                os.write(2, ("help me please %d\n" % rc2).encode())
+                os.close(w)
+                r = os.fdopen(r)
+                print(r.read())
+                sys.exit(1)
+
+
+           # ''''os.close(r)  # I'll pass my the output of one process to the input of another process with this       ``
+            #w = os.fdopen(w, 'w')
+            # w.write(userIN)
+            #w.close()
+            
+            #os.close(w)
+           # r = os.fdopen(r)
+           # print(r.read())''''
+
+            #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
+            # childPidCode).encode())
